af3b147a982570351b0df65b6b264794ab5a9daf
PachyuChepe <pachyuchepe@gmail.com>
2023-11-04 00:11:43 +0900

    feat: 스웨거 API 추가

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const fs = require("fs");
-const https = require("https");
 const express = require("express");
 const app = express();
 const env = require("./config/config");
 const productsSchema = require("./routes/productsRouter");
-
+const https = require("https");
+const fs = require("fs");
 const connect = require("./schemas");
 connect();
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const apiSpec = YAML.load("swagger.yaml");
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiSpec));
 
 app.get("/", (req, res) => {
   res.send("안녕하세요 세계!");
 });
-
 app.use("/api", [productsSchema]);
-
 // 인증서가 존재하면 HTTPS, 그렇지 않을 경우 HTTP
 let server;
 if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
