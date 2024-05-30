fccb5860990c701e4ec3ae9c78120d0abdae77b6
PachyuChepe <pachyuchepe@gmail.com>
2024-05-30 15:45:45 +0900

    [Delete] explanation_code: 기존 코드 제거

--- config/config.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  SERVER_PORT: process.env.SERVER_PORT || 3000,
-  MongoDB_LOCAL: `mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
-  MongoDB_ATLAS: `mongodb+srv://${process.env.MA_DB_ID}:${process.env.MA_DB_PW}@${process.env.MA_DB_ID}.ngu1v3b.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
-};
